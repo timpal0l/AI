546646d7419a16ac27862a26b62c638409765d4f
unoloop <niklas_andersen@hotmail.com>
2014-09-24 00:50:33 +0200

    2 problems
    Vans 2&3 does not move correctly (stops after vertical movement), van0 gets sent to pick up delivery at every iteration, should only happen once.

--- ailab1.cpp
+++ ailab1.cpp
@@ -63,8 +63,15 @@ vector<pair<int,int>> createRoute(Location initialPosition, Location targetPosit
 			initialPosition.second -= 1;
 		}
 	}
+	cout << "Route is: " << endl;
+	int routeLength = route.size()-1;
+	for (int i=0; i<routeLength; i++){
+		cout << route[i].first << " " << route[i].second << endl;
+	}
 	return route;
 }
+
+
 
 
 int _tmain(int argc, _TCHAR* argv[])
@@ -92,35 +99,118 @@ int _tmain(int argc, _TCHAR* argv[])
 	vector<DeliveryInfo> activeDeliveries;
 	vector<pair<int,int>> completedDeliveries;
 	wstring output;		
+	map <int,std::vector<std::pair<int,int>>> Instructions;	
 
 	// Start game, get info
 	dmc.startGame(nodes, output);
 	dmc.getInformation(Time, Edges, Vans, waitingDeliveries, activeDeliveries, completedDeliveries, output);
 
-	pair <int, int> loc = Vans[0].location;
-	pair <int, int> target(30,4);
-	vector<pair<int,int>> route0 = createRoute(loc, target);
-	map <int,std::vector<std::pair<int,int>>> Instructions;	
-	Instructions.insert(make_pair(0, route0));
-	dmc.sendInstructions(Instructions, output);
+	// 0 = not busy, 1 = picking up parcel, 2 = dropping off parcel
+	int busyVans[5] = {0,0,0,0,0};
 
+	int lastPickedUpCargo = -1;
+	int latestCargoBooked = -1;
+	pair <int, int> loc;
+	pair <int, int> target;
+	vector<pair<int,int>> route;
+
+	// Spread out vans (vans 2 & 3 does not move correctly)
+
+	loc = Vans[0].location;
+	target.first = 10; 
+	target.second = 10;
+	route = createRoute(loc, target);
+	Instructions.insert(make_pair(0, route));
+	target.first = 30;
+	target.second = 10;
+	route = createRoute(loc, target);
+	Instructions.insert(make_pair(1, route));
+	target.first = 10;
+	target.second = 30;
+	route = createRoute(loc, target);
+	Instructions.insert(make_pair(2, route));
+	target.first = 30;
+	target.second = 30;
+	route = createRoute(loc, target);
+	Instructions.insert(make_pair(3, route));
+	dmc.sendInstructions(Instructions, output);
+	
 	// Main loop. Get info, send instructions
-	while (true){													
-		
+	while (true){			
+
+		cout << " " << endl;
+		cout << "Last picked up cargo: " << lastPickedUpCargo << endl;
+		cout << "Latest cargo booked: " << latestCargoBooked << endl;
 		dmc.getInformation(Time, Edges, Vans, waitingDeliveries, activeDeliveries, completedDeliveries, output);
+		
+		cout << "Time: " << Time << endl;
+
+		cout << "Position of van0: " << Vans[0].location.first << " " << Vans[0].location.second << endl;
+		cout << "Position of van1: " << Vans[1].location.first << " " << Vans[1].location.second << endl;
+		cout << "Position of van2: " << Vans[2].location.first << " " << Vans[2].location.second << endl;
+		cout << "Position of van3: " << Vans[3].location.first << " " << Vans[3].location.second << endl;
+		cout << "Position of van4: " << Vans[4].location.first << " " << Vans[4].location.second << endl;
+
+		for (int i=0; i<5; i++){
+			if (!Vans[i].instructions.empty()){
+				cout << "Instructions for van" << i << ": " << Vans[i].instructions[0].first << " " << Vans[i].instructions[0].second << endl;
+			}
+			else{
+				busyVans[i] = 0;
+			}
+		}
+
+		// Is the createRoute function doing weird stuff? Instructions gets cleared for vans 2 and 3 once they have moved along the y-axis
 
 		if (!waitingDeliveries.empty()){
-			cout << "Waiting deliveries: Delivery #1: to be picked up at " << waitingDeliveries[0].pickUp.first << " " << waitingDeliveries[0].pickUp.second << endl;
+			cout << "Waiting deliveries number: " << waitingDeliveries[0].Number << endl;
+			// Check if nobody has been assigned this package
+			if (waitingDeliveries[0].Number > latestCargoBooked){
+				for (int x=0; x < waitingDeliveries.size()-1; x++){
+					cout << "Waiting delivery to be picked up at " << waitingDeliveries[x].pickUp.first << " " << waitingDeliveries[x].pickUp.second << endl;
+					for (int i=0; i<5; i++){
+						if (busyVans[i] == 0){
+							if (waitingDeliveries[waitingDeliveries.size()-1].Number > latestCargoBooked){
+								// send Van[i] to pick up parcel
+								loc = Vans[i].location;
+								target = waitingDeliveries[x].pickUp;
+								route = createRoute(loc, target);	
+								Instructions.insert(make_pair(i, route));
+								busyVans[i] = 1;
+								latestCargoBooked = waitingDeliveries[0].Number;
+								break;
+							}
+						}
+					}
+				}
+			}
 		}
 		if (!activeDeliveries.empty()){
-			cout << "Vans[0].cargo = " << Vans[0].cargo << endl;
+			lastPickedUpCargo = activeDeliveries[activeDeliveries.size()-1].Number;
+			for (int i=0; i<=activeDeliveries.size()-1; i++){
+				if (Vans[i].cargo == lastPickedUpCargo){
+					if (busyVans[i] == 2){
+						break;
+					}
+					busyVans[i] = 2;
+					loc = Vans[i].location;
+					for (int j=0; j<activeDeliveries.size()-1; j++){
+						if (lastPickedUpCargo == activeDeliveries[j].Number){
+							target = activeDeliveries[j].dropOff;
+							break;
+						}
+					route = createRoute(loc, target);
+					Instructions.insert(make_pair(i, route));
+					}
+				}
+			}
 			cout << "Active deliveries: Delivery #1 to be dropped off at " << activeDeliveries[0].dropOff.first << " " << activeDeliveries[0].dropOff.second << endl;
 			pair <int, int> loc = Vans[0].location;
 			pair <int, int> target = activeDeliveries[0].dropOff;
-			vector<pair<int,int>> route1 = createRoute(loc, target);
+			vector<pair<int,int>> route = createRoute(loc, target);
 			Instructions.clear();
-			Instructions.insert(make_pair(0, route1));
-			//cout << "Instructions[0][0]: inside activeDeliveries loop " <<  Instructions[0][0].first << " " << Instructions[0][0].second << endl;
+			Instructions.insert(make_pair(0, route));
+			cout << "Instructions[0][0]: inside activeDeliveries loop " <<  Instructions[0][0].first << " " << Instructions[0][0].second << endl;
 		}
 			
 		if (!completedDeliveries.empty()){
@@ -133,10 +223,3 @@ int _tmain(int argc, _TCHAR* argv[])
 		return 0;
 }
 
-
-		// wcout << "Van number: " << Vans[0].Number << "Van instructions: " << Vans[0].instructions[0].first << Vans[0].instructions[0].second << endl;	
-		// wcout << Vans[0].Number << Vans[0].location.first << Vans[0].location.second << endl;		
-		// std::pair <int, int> directions(40,20);
-		// route.push_back(directions);
-		// Instructions.insert(std::make_pair(0, route));
-		// cout << Instructions[0][0].second << endl;
