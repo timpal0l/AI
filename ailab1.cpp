2c5d125fd02e05d856bce49b168b413e6a3c6023
unoloop <niklas_andersen@hotmail.com>
2014-09-23 19:12:31 +0200

    Update ailab1.cpp

--- ailab1.cpp
+++ ailab1.cpp
@@ -31,18 +31,23 @@ Location goRight(Location loc){
 return loc;
 }
 
-void reset(vector<VanInfo>& Vans, map<int,vector<pair<int,int>>>& Instructions, vector<DeliveryInfo>& waitingDeliveries, vector<DeliveryInfo>& activeDeliveries, vector<std::pair<int,int>>& completedDeliveries, wstring& output, vector<vector<int>>& Edges){
-	Vans.clear();
-	Instructions.clear();
-	waitingDeliveries.clear();
-	activeDeliveries.clear();
-	completedDeliveries.clear();
-	output.clear();
-	Edges.clear();
+void reset(vector<VanInfo>& Vans, map<int,vector<pair<int,int>>>& Instructions, vector<DeliveryInfo>& waitingDeliveries, 
+	vector<DeliveryInfo>& activeDeliveries, vector<std::pair<int,int>>& completedDeliveries, wstring& output, vector<vector<int>>& Edges){
+		Vans.clear();
+		Instructions.clear();
+		waitingDeliveries.clear();
+		activeDeliveries.clear();
+		completedDeliveries.clear();
+		output.clear();
+		Edges.clear();
 }
 
-std::vector<std::pair<int,int>> createRoute(Location initialPosition, Location targetPosition) {
-	std::vector<std::pair<int,int>> route;
+// Print instructions for each van, just to get a clue
+void printInstructions(vector<VanInfo>& Vans)
+
+// Create route, returns a vector of locations
+vector<pair<int,int>> createRoute(Location initialPosition, Location targetPosition) {
+	vector<pair<int,int>> route;
 	while(initialPosition.first != targetPosition.first || initialPosition.second != targetPosition.second){
 		if(initialPosition.first < targetPosition.first){
 			route.push_back(goDown(initialPosition));
@@ -61,78 +66,78 @@ std::vector<std::pair<int,int>> createRoute(Location initialPosition, Location t
 			initialPosition.second -= 1;
 		}
 	}
-	for(int i = 0; i < route.size(); i++){
-		cout << route[i].first << ":" << route[i].second << endl;
-	}
 	return route;
 }
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 
 	const std::wstring group = L"LeGroup4";							// Group name for DM_Client()
 	bool OK;
-	std::wstring output;											// Output string for startGame()
 	
 	DM_Client dmc = DM_Client(group, OK);							// Initiate DM client
 	
+	// Why?
 	static const std::wstring hws = dmc.getHighayString();
 	static const std::wstring s = dmc.getSuburbanString();
 	static const std::wstring b = dmc.getBusinessDistrictString();
 	static const std::wstring cb = dmc.getCentralBusinessDistrictString();
 
-	
-	std::vector<std::vector<std::wstring>> nodes;					// create one vector to contain the node vectors
-	
+	// Initiate a vector to contain the node vectors
+	std::vector<std::vector<std::wstring>> nodes;					
+									
+	// Initiate everything needed for getInformation()
+	int Time = 360;													
+	vector<std::vector<int>> Edges;							
+	vector<VanInfo> Vans;
+	vector<DeliveryInfo> waitingDeliveries;
+	vector<DeliveryInfo> activeDeliveries;
+	vector<pair<int,int>> completedDeliveries;
+	wstring output;		
 
-
-	int Time = 360;													// Create all variables needed for getInformation()
-	std::vector<std::vector<int>> Edges;							
-	std::vector<VanInfo> Vans;
-	std::vector<DeliveryInfo> waitingDeliveries;
-	std::vector<DeliveryInfo> activeDeliveries;
-	std::vector<std::pair<int,int>> completedDeliveries;
-	//std::map<int,std::vector<std::pair<int,int>>> instructionsT;
-
-	
-
+	// Start game, get info
 	dmc.startGame(nodes, output);
 	dmc.getInformation(Time, Edges, Vans, waitingDeliveries, activeDeliveries, completedDeliveries, output);
 
+	pair <int, int> loc = Vans[0].location;
+	pair <int, int> target(30,4);
+	vector<pair<int,int>> route0 = createRoute(loc, target);
+	map <int,std::vector<std::pair<int,int>>> Instructions;	
+	Instructions.insert(make_pair(0, route0));
+	dmc.sendInstructions(Instructions, output);
 
-	while (true){													// Get info, send instructions
-
-		dmc.getInformation(Time, Edges, Vans, waitingDeliveries, activeDeliveries, completedDeliveries, output);
-
-
-		//wcout << "Van number: " << Vans[0].Number << "Van instructions: " << Vans[0].instructions[0].first << Vans[0].instructions[0].second << endl;	
-		//wcout << Vans[0].Number << Vans[0].location.first << Vans[0].location.second << endl;		
-		//std::pair <int, int> directions(40,20);
-		//route.push_back(directions);
-		//Instructions.insert(std::make_pair(0, route));
-		//cout << Instructions[0][0].second << endl;
-		std::vector<std::pair<int,int>> route;
-
-
-
-		std::map <int,std::vector<std::pair<int,int>>> Instructions;		
-
-
+	// Main loop. Get info, compute route, send instructions
+	while (true){													
+		
 
 		if (!waitingDeliveries.empty()){
-			cout << "Waiting deliveries: " << endl;
-			cout << waitingDeliveries[0].pickUp.first << " " << waitingDeliveries[0].pickUp.second << endl;
-			if (waitingDeliveries.size() > 1){
-				cout << waitingDeliveries[1].pickUp.first << " " << waitingDeliveries[1].pickUp.second << endl;
-			}
+			cout << "Waiting deliveries: Delivery #1: to be picked up at " << waitingDeliveries[0].pickUp.first << " " << waitingDeliveries[0].pickUp.second << endl;
 		}
-		dmc.sendInstructions(Instructions, output);
-
+		if (!activeDeliveries.empty()){
+			cout << "Vans[0].cargo = " << Vans[0].cargo << endl;
+			cout << "Active deliveries: Delivery #1 to be dropped off at " << activeDeliveries[0].dropOff.first << " " << activeDeliveries[0].dropOff.second << endl;
+			pair <int, int> loc = Vans[0].location;
+			pair <int, int> target = activeDeliveries[0].dropOff;
+			vector<pair<int,int>> route1 = createRoute(loc, target);
+			Instructions.clear();
+			Instructions.insert(make_pair(0, route1));
+		}
+			
+		if (!completedDeliveries.empty()){
+			cout << "Completed delivery! " << endl;
+		}
 		// Clear everything before next iteration
+		dmc.sendInstructions(Instructions, output);
 		reset(Vans, Instructions, waitingDeliveries, activeDeliveries, completedDeliveries, output, Edges);
-
-		}
-
-
+	}
 		return 0;
 }
+
+
+		// wcout << "Van number: " << Vans[0].Number << "Van instructions: " << Vans[0].instructions[0].first << Vans[0].instructions[0].second << endl;	
+		// wcout << Vans[0].Number << Vans[0].location.first << Vans[0].location.second << endl;		
+		// std::pair <int, int> directions(40,20);
+		// route.push_back(directions);
+		// Instructions.insert(std::make_pair(0, route));
+		// cout << Instructions[0][0].second << endl;
