b10d4eef9bcc17053596fb99a7545a1af40b0157
unoloop <niklas_andersen@hotmail.com>
2014-09-24 18:43:28 +0200

    Update ailab1.cpp
    created a getNode() type function, for the a* algorithm

--- ailab1.cpp
+++ ailab1.cpp
@@ -9,28 +9,94 @@
 #include <time.h>
 #include <stdlib.h>
 
-
 using namespace std;
 
+struct Node{
+	Node *parent;
+	Location location;
+	int f, g, h;
+};
 
-
-Location goDown(Location loc){
+// Returns Edge
+pair<int, int> goDown(Location loc){
 	loc.first = 2 * (loc.first) + 1;
 	return loc;
 }
-Location goUp(Location loc){
+pair<int, int> goUp(Location loc){
 	loc.first = 2 * (loc.first) - 1;
 	return loc;
 }
-Location goLeft(Location loc){
+pair<int, int> goLeft(Location loc){
 	loc.first = 2 * (loc.first);
 	loc.second = loc.second - 1;
-return loc;
+	return loc;
 }
-Location goRight(Location loc){
+pair<int, int> goRight(Location loc){
 	loc.first = 2 * (loc.first);
 	loc.second = loc.second;
-return loc;
+	return loc;
+}
+
+vector<pair<int, int>> aStarRoute(Location start, Location goal, vector<vector<int>> &Edges){
+	/*initialize the open and the closed vectors */
+	vector<Node> open;
+	vector<Node> closed;
+	vector<pair<int, int>> route;
+	Node currentNode;
+	currentNode.location = start;
+	currentNode.f = 0;
+	pair <int, int> rightEdge, leftEdge, topEdge, bottomEdge, cheapestEdge;
+	int rightCost, leftCost, topCost, bottomCost, cheapestCost;
+	open.push_back(currentNode);
+	while(!open.empty()){
+
+
+
+
+		//Get the node off the open list with the lowest f and call it currentNode
+		if((start.first == 0) && (start.second == 0)){
+			/* topLeft */
+			/* check weight of down and right only */
+			pair<int,int> rightEgde = goRight(start);
+		}
+
+		if((start.first == 40) && (start.second == 40)){
+			leftEdge = goLeft(start);
+			topEdge = goUp(start);
+			leftCost = Edges[leftEdge.first][leftEdge.second];
+			topCost = Edges[topEdge.first][topEdge.second];
+			if (leftCost < topCost){
+				cheapestCost = leftCost;
+				cheapestEdge = leftEdge;
+			}
+			else{
+				cheapestCost = topCost;
+				cheapestEdge = topEdge;
+			}
+			
+		}
+	}
+	return route;
+}
+
+
+Node createNode(int parentFCost, Location location, Location neighbor, Location mainGoal, vector<vector<int>> &Edges){
+	Node newNode;
+	if (location.first < neighbor.first){
+		newNode.g = Edges[goDown(location).first][goDown(location).second];
+	}
+	else if (location.first > neighbor.first){
+		newNode.g = Edges[goUp(location).first][goUp(location).second];
+	}
+	else if (location.second < neighbor.second){
+		newNode.g = Edges[goRight(location).first][goRight(location).second];
+	}
+	else if (location.second > neighbor.second){
+		newNode.g = Edges[goLeft(location).first][goLeft(location).second];
+	}
+	newNode.f = parentFCost + g;
+	newNode.h = createRoute(neighbor, mainGoal).size();
+	return newNode;
 }
 
 void reset(vector<VanInfo>& Vans, map<int,vector<pair<int,int>>>& Instructions, vector<DeliveryInfo>& waitingDeliveries, 
@@ -40,9 +106,31 @@ void reset(vector<VanInfo>& Vans, map<int,vector<pair<int,int>>>& Instructions, 
 		waitingDeliveries.clear();
 		activeDeliveries.clear();
 		completedDeliveries.clear();
+
 		output.clear();
 		Edges.clear();
 }
+
+int closestVan(Location loc, vector<VanInfo> Vans, int busyVans[]){
+	int closestVan = -1;
+	int closestDistance = 100000;
+	int tempDistance = -1;
+	for(int i = 0; i < 5; i++){
+		if(busyVans[i] == 0){
+			tempDistance = ((abs(loc.first - Vans[i].location.first)) + (abs(loc.second - Vans[i].location.second)));
+			if (tempDistance < closestDistance){
+				closestVan = Vans[i].Number;
+				closestDistance = tempDistance;
+			}
+		}
+	}
+	return closestVan;
+}
+
+
+
+
+
 
 // Create route, returns a vector of locations
 vector<pair<int,int>> createRoute(Location initialPosition, Location targetPosition) {
@@ -81,9 +169,9 @@ int _tmain(int argc, _TCHAR* argv[])
 	bool OK;
 
 	srand (time(NULL));
-	
+
 	DM_Client dmc = DM_Client(group, OK);							// Initiate DM client
-	
+
 	// Why?
 	static const std::wstring hws = dmc.getHighayString();
 	static const std::wstring s = dmc.getSuburbanString();
@@ -92,7 +180,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// Initiate a vector to contain the node vectors
 	std::vector<std::vector<std::wstring>> nodes;					
-									
+
 	// Initiate everything needed for getInformation()
 	int Time = 360;													
 	vector<std::vector<int>> Edges;							
@@ -109,8 +197,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// 0 = not busy, 1 = picking up parcel, 2 = dropping off parcel
 	int busyVans[5] = {0,0,0,0,0};
+	int lastCargoNumber[5] = {-1,-1,-1,-1,-1};
 
-	int lastPickedUpCargo = -1;
 	int latestCargoBooked = -1;
 	pair <int, int> loc;
 	pair <int, int> target;
@@ -136,21 +224,71 @@ int _tmain(int argc, _TCHAR* argv[])
 	route = createRoute(loc, target);
 	Instructions.insert(make_pair(3, route));
 	dmc.sendInstructions(Instructions, output);
-	bool sendInstr = false;
-
 
 	// Main loop. Get info, send instructions
 	while (true){			
 
 		cout << " " << endl;
-		cout << "Last picked up cargo: " << lastPickedUpCargo << endl;
 		cout << "Latest cargo booked: " << latestCargoBooked << endl;
 		dmc.getInformation(Time, Edges, Vans, waitingDeliveries, activeDeliveries, completedDeliveries, output);
-		
+
 		cout << "Time: " << Time << endl;
 
 		if (!waitingDeliveries.empty()){
-			cout << "Waiting delivery at node: " << waitingDeliveries[0].pickUp.first << " " << waitingDeliveries[0].pickUp.second << endl;
+			cout << "Waiting delivery at node: ";
+			for(int i = 0; i < waitingDeliveries.size(); i++){
+				cout << "{" << waitingDeliveries[i].pickUp.first << "," << waitingDeliveries[i].pickUp.second << "} ";
+				if(waitingDeliveries[i].Number > latestCargoBooked){
+					int x = closestVan(waitingDeliveries[i].pickUp, Vans, busyVans);
+					if(x == -1){
+						break;
+					}
+					loc = Vans[x].location;
+					target = waitingDeliveries[i].pickUp;
+					route = createRoute(loc, target);	
+					Instructions.insert(make_pair(x, route));
+					busyVans[x] = 1;
+					latestCargoBooked = waitingDeliveries[i].Number;
+				}
+			}
+			cout << endl;
+		}
+
+		if(!activeDeliveries.empty()){
+			cout << "Drop off cargo at node: ";
+			for(int i = 0; i < activeDeliveries.size(); i++){
+				cout  << "{" << activeDeliveries[i].dropOff.first << "," << activeDeliveries[i].dropOff.second << "} "; 
+				for(int x = 0; x < 5; x++){
+					if(busyVans[x] == 1 && Vans[x].cargo == activeDeliveries[i].Number){
+						loc = Vans[x].location;
+						target = activeDeliveries[i].dropOff;
+						route = createRoute(loc, target);	
+						Instructions.insert(make_pair(x, route));
+						busyVans[x] = 2;
+						lastCargoNumber[x] = activeDeliveries[i].Number;
+						break;
+					}
+				}
+			}
+			cout << endl;
+		}
+
+		if(!completedDeliveries.empty()){
+			cout << "Completed delivery:  ";
+			for(int i = 0; i < completedDeliveries.size(); i++){
+				cout << completedDeliveries[i].first << ", ";
+				for(int x = 0; x < 5; x++){
+					if(lastCargoNumber[x] == completedDeliveries[i].first){
+						busyVans[x] = 0;
+						lastCargoNumber[x] = -1;
+					}
+				}
+			}
+			cout << endl;
+			if(completedDeliveries.size() == 20){
+				cout << "ALL PARCELS DELIVERED!" << endl;
+				break;
+			}
 		}
 
 		cout << "Position of van0: " << Vans[0].location.first << " " << Vans[0].location.second << endl;
@@ -161,78 +299,12 @@ int _tmain(int argc, _TCHAR* argv[])
 
 		cout << "busyVans 0 to 5 status: " << busyVans[0] << " " << busyVans[1] << " " << busyVans[2] << " " << busyVans[3] << " " << busyVans[4] << endl;
 
-		for (int i=0; i<5; i++){
-			if (!Vans[i].instructions.empty()){
-				cout << "Instructions for van" << i << ": " << Vans[i].instructions[0].first << " " << Vans[i].instructions[0].second << endl;
-			}
-			else if (Vans[i].cargo > -1){
-				break;
-			}
-			else{
-				busyVans[i] = 0;
-			}
-		}
-
-		// Is the createRoute function doing weird stuff? Instructions gets cleared for vans 2 and 3 once they have moved along the y-axis
-
-		if (!waitingDeliveries.empty()){
-			cout << "Waiting deliveries number: " << waitingDeliveries[0].Number << endl;
-			// Check if nobody has been assigned this package
-			if (waitingDeliveries[0].Number > latestCargoBooked){
-				for (int x=0; x < waitingDeliveries.size()-1; x++){
-					cout << "Waiting delivery to be picked up at " << waitingDeliveries[x].pickUp.first << " " << waitingDeliveries[x].pickUp.second << endl;
-					for (int i=0; i<5; i++){
-						if (busyVans[i] == 0){
-							if (waitingDeliveries[waitingDeliveries.size()-1].Number > latestCargoBooked){
-								// send Van[i] to pick up parcel
-								loc = Vans[i].location;
-								target = waitingDeliveries[x].pickUp;
-								route = createRoute(loc, target);	
-								Instructions.insert(make_pair(i, route));
-								sendInstr = true;
-								busyVans[i] = 1;
-								latestCargoBooked = waitingDeliveries[0].Number;
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-		if (!activeDeliveries.empty()){
-			lastPickedUpCargo = activeDeliveries.back().Number;
-			for (int i=0; i<=20; i++){
-				if (Vans[i].cargo == lastPickedUpCargo){
-					if (busyVans[i] == 2){
-						break;
-					}
-					busyVans[i] = 2;
-					loc = Vans[i].location;
-					for (int j=0; j<activeDeliveries.size()-1; j++){
-						if (lastPickedUpCargo == activeDeliveries[j].Number){
-							target = activeDeliveries[j].dropOff;
-							break;
-						}
-					route = createRoute(loc, target);
-					Instructions.insert(make_pair(i, route));
-					sendInstr = true;
-					}
-				}
-			}
-			cout << "Active deliveries: Delivery #1 to be dropped off at " << activeDeliveries[0].dropOff.first << " " << activeDeliveries[0].dropOff.second << endl;
-			cout << "Instructions[0][0]: inside activeDeliveries loop " <<  Instructions[0][0].first << " " << Instructions[0][0].second << endl;
-		}
-			
-		if (!completedDeliveries.empty()){
-			cout << "Completed delivery! " << endl;
-		}
 		// Clear everything before next iteration
-		if (sendInstr){
+		if (!Instructions.empty()){
 			dmc.sendInstructions(Instructions, output);
 		}
-		sendInstr = false;
 		reset(Vans, Instructions, waitingDeliveries, activeDeliveries, completedDeliveries, output, Edges);
 	}
-		return 0;
+	return 0;
 }
 
